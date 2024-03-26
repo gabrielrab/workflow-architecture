e9f589bfd487a3730cfdcf9bb38e1646b30386a3
Gabriel Rabelo <rabelogabriel72@gmail.com>
2024-03-26 11:58:53 -0300

    feat: example on index

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+//// Write your logic and import your workflows and data below
+//// Example:
+
+// import ExampleWorkflow from "workflows/ExampleWorkflow";
+
+// const data: GlobalContext = {
+//   name: "Workflow Architecture",
+// };
+
+// const exampleWorkflowResult = new ExampleWorkflow().run(data);
+console.log("Finish script");
